8b4fa3b8c8b62f59049a279478d90ebbe0d2bd53
Julia <kurkinajulia@gmail.com>
2023-02-16 16:28:59 +0300

    zadacha1

---
+++ zadacha1/Program.cs
@@ -0,0 +1,16 @@
+﻿int ShowSecond(int value)
+{
+int result = value / 10 % 10;
+return result;
+}
+Console.WriteLine("Введите трёхзначное число: ");
+
+int value = Convert.ToInt32(Console.ReadLine());
+
+if (value < 100 || value > 999)
+{
+Console.WriteLine("Вы ввели не трёхзначное число, повторите ввод");
+return;
+}
+
+Console.WriteLine(ShowSecond(value));
